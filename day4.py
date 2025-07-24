10fe9769dcb7b89c3038bb3d1481ce75df009cc7
Naresh302003 <nareshsknareshkumar@gmail.com>
2025-07-24 22:32:48 +0530

    day4.py added

--- day4.py
+++ day4.py
@@ -21,39 +21,92 @@ import day3
 # else:
 #     print("Tails")
 
-indian_states = [
-    "Andhra Pradesh",
-    "Arunachal Pradesh",
-    "Assam",
-    "Bihar",
-    "Chhattisgarh",
-    "Goa",
-    "Gujarat",
-    "Haryana",
-    "Himachal Pradesh",
-    "Jharkhand",
-    "Karnataka",
-    "Kerala",
-    "Madhya Pradesh",
-    "Maharashtra",
-    "Manipur",
-    "Meghalaya",
-    "Mizoram",
-    "Nagaland",
-    "Odisha",
-    "Punjab",
-    "Rajasthan",
-    "Sikkim",
-    "Tamil Nadu",
-    "Telangana",
-    "Tripura",
-    "Uttar Pradesh",
-    "Uttarakhand",
-    "West Bengal"
-]
+# indian_states = [
+#     "Andhra Pradesh",
+#     "Arunachal Pradesh",
+#     "Assam",
+#     "Bihar",
+#     "Chhattisgarh",
+#     "Goa",
+#     "Gujarat",
+#     "Haryana",
+#     "Himachal Pradesh",
+#     "Jharkhand",
+#     "Karnataka",
+#     "Kerala",
+#     "Madhya Pradesh",
+#     "Maharashtra",
+#     "Manipur",
+#     "Meghalaya",
+#     "Mizoram",
+#     "Nagaland",
+#     "Odisha",
+#     "Punjab",
+#     "Rajasthan",
+#     "Sikkim",
+#     "Tamil Nadu",
+#     "Telangana",
+#     "Tripura",
+#     "Uttar Pradesh",
+#     "Uttarakhand",
+#     "West Bengal"
+# ]
 
-# print(len(indian_states[::-1]))
-indian_states[0] = "TamilNadu"
-indian_states.append("New Jersey")
-indian_states.extend(["Dubai","Georgia","Delaware"])
-print(indian_states[::])
+# # print(len(indian_states[::-1]))
+# indian_states[0] = "TamilNadu"
+# indian_states.append("New Jersey")
+# indian_states.extend(["Dubai","Georgia","Delaware"])
+# print(indian_states[::])
+
+
+import random
+print("Welcome to Calculator Application")
+rock = '''  
+  _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = ''' 
+    _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissor = '''  
+  _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+list_game = [rock,paper,scissor]
+user_choice = int(input("Enter your choice: 0 for rock, 1 for paper, 2 for scissor: "))
+if user_choice >= 0 and user_choice <= 2:
+    print("You chose: ",list_game[user_choice])
+else:
+    print("Invalid choice")
+
+
+computer_choice = random.randint(0,2)
+if user_choice >= 0 and user_choice <= 2:
+    print("Computer chose: ",list_game[computer_choice])
+else:
+    print("computer: i can't recognize what you did choose")
+
+
+if user_choice < 0 or user_choice > 2:
+    print("")
+elif user_choice == computer_choice:
+    print("draw")
+elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+    print("User wins")
+else:
+    print("Computer wins")
