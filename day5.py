7987651ac8151baa6de5e744516b02b867a89b4c
Naresh302003 <nareshsknareshkumar@gmail.com>
2025-07-25 13:07:42 +0530

    added

--- day5.py
+++ day5.py
@@ -1,13 +1,33 @@
-# Loops 
+# # Loops 
 
 
-# sum
-student_marks = [90,55,33,22,11,9,8,5,1,55,100,200,300,22,5]
-total_marks = sum(student_marks)
-print(total_marks)
+# # sum
+# student_marks = [90,55,33,22,11,9,8,5,1,55,100,200,300,22,5]
+# total_marks = sum(student_marks)
+# print(total_marks)
 
-#Manual method
-sum = 0
-for score in student_marks:
-    sum += score
-print(f"Manual method sum: {sum}")
+# #Manual method
+# sum = 0
+# for score in student_marks:
+#     sum += score
+# print(f"Manual method sum: {sum}")
+
+# # max
+
+# print(max(student_marks))
+
+# #Manual method
+# mx_score = 0
+# for score in student_marks:
+#     if score > mx_score:
+#         mx_score = score
+# print(f"Manual method max score: {mx_score}")
+
+# Range
+print(list(range(1,11)))
+
+sum1 = 0
+for i in range(1,21):
+    sum1 += i
+print(sum1)
+
