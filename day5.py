27762727108630fbd1cdff226262428b4a4a0211
Naresh302003 <nareshsknareshkumar@gmail.com>
2025-07-26 03:52:12 +0530

    added

--- day5.py
+++ day5.py
@@ -53,34 +53,53 @@
 import random
 import string
 
-def password_generator(letters,symbols,digits):
-    password = ""
-    characters = string.ascii_letters
-    punctuation = ['!','#','$','%','(',')','*','+']
-    numbers = string.digits
+# def password_generator(letters,symbols,digits):
+#     password_list = []
+#     characters = string.ascii_letters
+#     punctuation = ['!','#','$','%','(',')','*','+']
+#     numbers = string.digits
 
-    for char in range(1,letters + 1):
-        password += random.choice(characters)
-    for char in range(1,symbols + 1):
-        password += random.choice(punctuation)
-    for char in range(1,digits +1):
-        password += random.choice(numbers)
-    return password 
+#     for char in range(0,letters + 1):
+#         password_list.append(random.choice(characters))
+#     for char in range(0,symbols + 1):
+#         password_list.append(random.choice(punctuation))
+#     for char in range(0,digits +1):
+#         password_list.append(random.choice(numbers))
+#     return password_list
     
 
 
-print("Password Generator \U0001F512")
-try:
-    letters = int(input("Enter how many letters do you want your password: "))
-    symbols  = int(input("Enter how many symbols do you want in your password: "))
-    digits = int(input("Enter how many digits do you want in your password: "))
-    if (letters < 3) and (symbols < 1) and (digits < 1):
-        print("Password length should be more then 3 letters and more then 1 symbols and more then 1 digits.")
-    else:
-        print("Generated password:",password_generator(letters,symbols,digits))
+# print("Password Generator \U0001F512")
+# try:
+#     letters = int(input("Enter how many letters do you want your password: "))
+#     symbols  = int(input("Enter how many symbols do you want in your password: "))
+#     digits = int(input("Enter how many digits do you want in your password: "))
+#     if (letters < 3) and (symbols < 1) and (digits < 1):
+#         print("Password length should be more then 3 letters and more then 1 symbols and more then 1 digits.")
+#     else:
+#         password = password_generator(letters,symbols,digits)
+#         print("******************************************")
+#         print("Before shuffle:","".join(password))
+#         random.shuffle(password)
+#         print("Generated password:","".join(password))
+#         print("******************************************")
 
-except ValueError:
-    print("Enter valid number.")
+# except ValueError:
+#     print("Enter valid number.")
+
+
+first_name = []
+birth_date = []
+password = []
+first_name = input("Enter your name: ")
+birth_date = input("Enter your date of birth year: ")
+length = int(input("Enter your password length you need: "))
+
+
+for i in range(1, length+1):
+    password.append(random.choice(first_name + str(birth_date)))
+random.shuffle(password)
+print("".join(password))
 
 
 
