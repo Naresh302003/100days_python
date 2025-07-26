a5ca79516169c4c21b6e9bda3e68b8997a6ab016
Naresh302003 <nareshsknareshkumar@gmail.com>
2025-07-26 14:50:43 +0530

    added

---
+++ day6.py
@@ -0,0 +1,6 @@
+''' Indentation
+    in python, indentation used to define blocks of code, 
+    it tells to python interpreter that group of statement is belongs to specific blocks
+    All statement with same level of indention considered to part of the same blocks. '''
+
+ 
